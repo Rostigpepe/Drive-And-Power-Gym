b11c4f9bbf7197b18b56263aa98b468f356dd5d2
Rostigpepe <Robin.olivemark@gmail.com>
2021-12-14 16:56:55 +0100

    Added comment

--- src/utility/drivepowergym/Main.java
+++ src/utility/drivepowergym/Main.java
@@ -2,6 +2,8 @@ package utility.drivepowergym;
 
 public class Main {
 
+    /**Main method, we'll see where this one goes
+     */
     public static void main(String[] args) {
 	    Program.run();
     }
